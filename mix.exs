ef890f4b7eff2fda527ca50c06210139903983b4
HoffsMH <hecker.matthew@gmail.com>
2018-07-16 20:10:30 -0400

    get initial version of mix.exs into order

--- mix.exs
+++ mix.exs
@@ -1,13 +1,19 @@
 defmodule JokenPlug.MixProject do
   use Mix.Project
 
+  def description do
+    "An Extensible JWT plug, Built on top of Joken"
+  end
+
   def project do
     [
       app: :joken_plug,
       version: "0.1.0",
       elixir: "~> 1.6",
+      description: description(),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
@@ -21,10 +27,17 @@ defmodule JokenPlug.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:joken , "~> 1.5.0"},
+      {:joken, "~> 1.5.0"},
       {:poison, "~> 3.1.0"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  defp package do
+    [
+      files: ~w(lib mix.exs),
+      maintainers: ["Matt Hecker"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/HoffsMH/joken-plug"}
     ]
   end
 end
